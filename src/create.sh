7388ab785bc4b4ceb7c72c52de7f4937e4e4bdfd
todoggler <kentokuraCS@gmail.com>
2022-03-21 04:41:07 +0900

    update(create.sh): 言語選択とテストファイル数を指定可能にする

--- src/create.sh
+++ src/create.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 DEFAULT_WORK_NAME="procon"
+DEFAULT_INPUT_DIR="stdin"
+DEFAULT_EXPECT_DIR="expect"
+ENABLE_LANGUAGES="python:py php:php" #langName:langExt の形式
 
 # 作業名を正規化します。
 # WORK_NAMEが無効な値ならDEFAULT_WORK_NAMEを使います。
 # 同一の名前が作業ディレクトリがあるならsuffixを付けます。
 # @param {string} WORK_NAME - 作業名
 # @return {string} - 正規化した作業名を返します。
-function useWorkName() {
+function normalizeWorkName() {
   work_name=$1
   # デフォルト作業名を使うかどうか
   if [ -z "$work_name" ]; then
@@ -21,24 +24,57 @@ function useWorkName() {
   echo "$work_name"
 }
 
-# makeWorkDirectories 作業ファイル群を作成する TODO: py以外にも対応する
-function makeWorkDirectories {
-  USE_WORK_NAME=`useWorkName $1`
-  mkdir -p ${USE_WORK_NAME}
-  cd $USE_WORK_NAME
-  touch ${USE_WORK_NAME}.py
-  mkdir -p stdin
-  touch stdin/1.in
-  touch stdin/2.in
-  touch stdin/3.in
-  mkdir -p expect
-  touch expect/1.out
-  touch expect/2.out
-  touch expect/3.out
+# LANG_NAME をその拡張子に変換します
+# @param {python|php} LANG_NAME - 利用言語名 
+# @return {py|php} - スクリプト拡張子
+function langName2LangExt() {
+  lang_name=$1
+  is_find=false
+  for enable_lang in $ENABLE_LANGUAGES
+  do
+    enable_lang_name=`echo $enable_lang | cut -f 1 -d ":"`
+    enable_lang_ext=`echo $enable_lang | cut -f 2 -d ":"`
+    if [ $lang_name = $enable_lang_name ]; then
+      echo $enable_lang_ext
+      is_find=true
+    fi
+  done
+  if [ "$is_find" = "false" ]; then
+    exit 1
+  fi
+}
+
+# 作業ファイル群を作成します。
+# @param {string} WORK_NAME - 作業名
+# @param {string} SELECT_LANG - 利用言語 python | php
+# @param {int} TEST_NUM - 作成するテストファイル数
+function makeWorkDirectories() {
+  work_name=$1
+  work_name=`normalizeWorkName $work_name`
+  select_lang=$2
+  select_lang_ext=`langName2LangExt $select_lang`
+  test_num=$3
+
+  mkdir -p $work_name
+  cd $work_name
+  touch "${work_name}.${select_lang_ext}"
+
+  mkdir -p $DEFAULT_INPUT_DIR
+  mkdir -p $DEFAULT_EXPECT_DIR
+  for i in `seq 1 $test_num`;
+  do
+    touch "${DEFAULT_INPUT_DIR}/${i}.in"
+    touch "${DEFAULT_EXPECT_DIR}/${i}.out"
+  done
   return 0
 }
 
 ################################################################################
 
 # create.sh は 解答環境を作成します。
-makeWorkDirectories $1
+# 作業ファイル群を作成します。
+# @param {string} WORK_NAME - 作業名
+# @param {string} SELECT_LANG - 利用言語 python | php
+# @param {int} TEST_NUM - 作成するテストファイル数
+# @return {int} - 正常終了なら0, そうでないなら1を返します。
+makeWorkDirectories $1 python 3
